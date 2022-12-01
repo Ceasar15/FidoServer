53acab730f72eb73fee1b95bfd0ce09edfccf803
Ceasar15 <ceasarkwadwo@gmail.com>
2022-12-01 16:36:36 +0000

    serverless deployment to lambda

--- fido_server/api_methods/utils/request_callback.py
+++ fido_server/api_methods/utils/request_callback.py
@@ -9,19 +9,13 @@ SUPPORT_CENTER = +233303898989
 def request_user_callback(phone):
 
     message = "You will soon get a call from customer support on: +%s" % SUPPORT_CENTER
- 
-    now = str(datetime.now(tz=None))
-    new_data = {
-        "phone": phone,
-        "datetime": now
-    }
     
-    #  update records for requested callbacks
-    with open('request_callback.json', 'r+', encoding='utf-8') as file:
-        file_data = json.load(file)
-        file_data["request_callback"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, ensure_ascii=False, indent=4)
+    # #  update records for requested callbacks
+    # with open('request_callback.json', 'r+', encoding='utf-8') as file:
+    #     file_data = json.load(file)
+    #     file_data["request_callback"].append(new_data)
+    #     file.seek(0)
+    #     json.dump(file_data, file, ensure_ascii=False, indent=4)
     
     #  send an sms to confirm
     with sms.get_connection() as connection:
@@ -31,3 +25,4 @@ def request_user_callback(phone):
             [phone],
             connection=connection,
         ).send()
+    return True
--- fido_server/api_methods/utils/send_sms.py
+++ fido_server/api_methods/utils/send_sms.py
@@ -8,9 +8,10 @@ import requests
 from sms.backends.base import BaseSmsBackend
 
 
-api_key = os.getenv("API_KEY")
-sender_id = os.getenv("SENDER_ID")
-
+# api_key = os.getenv("API_KEY")
+# sender_id = os.getenv("SENDER_ID")
+api_key = API_KEY="brmr2O8f8vQqibMs0iN1aLrYrX9uAlVF"
+sender_id =SENDER_ID="FidoTest"
 
 class CustomSmsBackend(BaseSmsBackend):
     def __init__(self, fail_silently: bool = False,*args, **kwargs) -> None:
@@ -33,14 +34,14 @@ class CustomSmsBackend(BaseSmsBackend):
 
         # prepare your url
         response = requests.get('https://sms.textcus.com/api/send?', params=params)
-        content =  response.json()["status"]
-        # content contains the response from TextCus
-        try:
-            if content != "0000":
-                raise ProcessNotFinished
-        except Exception:
-            raise ProcessNotFinished
-        return content
+        # content =  response.json()["status"]
+        # # content contains the response from TextCus
+        # try:
+        #     if content != "0000":
+        #         raise ProcessNotFinished
+        # except Exception:
+        #     raise ProcessNotFinished
+        return response.text
 
 
 def send_otp_sms(phone):
