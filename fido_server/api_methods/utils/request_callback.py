ec9ba24fc9a3f063b3d3511d31f05bf463f3c7dc
Ceasar15 <ceasarkwadwo@gmail.com>
2022-12-01 01:29:26 +0000

    record requested callbacks for support

--- fido_server/api_methods/utils/request_callback.py
+++ fido_server/api_methods/utils/request_callback.py
@@ -1,20 +1,29 @@
 from api_methods.models import Client
+import json
 import sms
+from datetime import datetime
 
 SUPPORT_CENTER = +233303898989
 
 
 def request_user_callback(phone):
 
-    
-
     message = "You will soon get a call from customer support on: +%s" % SUPPORT_CENTER
+ 
+    now = str(datetime.now(tz=None))
+    new_data = {
+        "phone": phone,
+        "datetime": now
+    }
     
-    # try:
-    #     client = Client.objects.get(phone_number=phone)
-    # except Client.DoesNotExist:
-    #     raise UserDataNotFound
-        
+    #  update records for requested callbacks
+    with open('request_callback.json', 'r+', encoding='utf-8') as file:
+        file_data = json.load(file)
+        file_data["request_callback"].append(new_data)
+        file.seek(0)
+        json.dump(file_data, file, ensure_ascii=False, indent=4)
+    
+    #  send an sms to confirm
     with sms.get_connection() as connection:
         sms.Message(
             message,
